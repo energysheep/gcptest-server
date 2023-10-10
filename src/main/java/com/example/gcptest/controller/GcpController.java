d3b414156314022c5b3fa19564679f707b807cee
tjdanqkr <tjdanqkr@gmail.com>
2023-10-10 10:22:42 +0900

    sert

--- src/main/java/com/example/gcptest/controller/GcpController.java
+++ src/main/java/com/example/gcptest/controller/GcpController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin({"http://localhost:3000", "http://34.30.119.210"})
 public class GcpController {
     private final GcpService gcpService;
     @PostMapping
