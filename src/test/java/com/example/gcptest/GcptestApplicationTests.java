81e0076320b4aac04e7ead021cd840d08478f9e5
tjdanqkr <49061542+tjdanqkr@users.noreply.github.com>
2023-10-11 10:42:15 +0900

    Update GcptestApplicationTests.java

--- src/test/java/com/example/gcptest/GcptestApplicationTests.java
+++ src/test/java/com/example/gcptest/GcptestApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.gcptest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//
 @SpringBootTest
 class GcptestApplicationTests {
 
